0a0f109dc6154e20d02660436d15f0ea5b076d4e
Zoltan Tamasi <mail@zoltantamasi.net>
2021-03-21 10:28:09 +0100

    Solved for cases under 100.

--- js-numerals/src/convert.js
+++ js-numerals/src/convert.js
@@ -1,30 +1,65 @@
 
 const ones = (num) => (
-    num === '1' ? 'one' :
-    num === '2' ? 'two' :
-    num === '3' ? 'three' :
-    num === '4' ? 'four' :
-    num === '5' ? 'five' :
-    num === '6' ? 'six' :
-    num === '7' ? 'seven' :
-    num === '8' ? 'eight' :
-    num === '9' ? 'nine' : ''
+  num === '1' ? 'one' :
+  num === '2' ? 'two' :
+  num === '3' ? 'three' :
+  num === '4' ? 'four' :
+  num === '5' ? 'five' :
+  num === '6' ? 'six' :
+  num === '7' ? 'seven' :
+  num === '8' ? 'eight' :
+  num === '9' ? 'nine' : ''
+);
+
+const teens = (num) => (
+  num === '0' ? 'ten' :
+  num === '1' ? 'eleven' :
+  num === '2' ? 'twelve' :
+  num === '3' ? 'thirteen' :
+  num === '4' ? 'fourteen' :
+  num === '5' ? 'fifteen' :
+  num === '6' ? 'sixteen' :
+  num === '7' ? 'seventeen' :
+  num === '8' ? 'eighteen' :
+  num === '9' ? 'nineteen' : ''
+);
+
+const tens = (num) => (
+  num === '0' ? '' :
+  num === '1' ? 'ten' :
+  num === '2' ? 'twenty' :
+  num === '3' ? 'thirty' :
+  num === '4' ? 'forty' :
+  num === '5' ? 'fifty' :
+  num === '6' ? 'sixty' :
+  num === '7' ? 'seventy' :
+  num === '8' ? 'eighty' :
+  num === '9' ? 'ninety' : ''
 );
 
 const convert = (num) => {
 
-    const numString = num.toString().split('');
-    const [head, ...tail] = numString;
+  if ((typeof num) !== 'number') {
+    throw new TypeError('parameter must be numeric');
+  }
 
-    if (numString.length > 3) {
-    } else if (numString.length === 3) {
-        return ones(head) + ' hundred and ' + convert(tail);
-    } else if (numString.length === 2) {
-        return tens(head) + tail === '0' ? '' : '-' + ones(tail);
-    } else {
-        return ones(head);
+  if (num === 0) return 'zero';
+
+  const numString = num.toString().split('');
+  const [head, ...tail] = numString;
+
+  if (numString.length > 3) {
+  } else if (numString.length === 3) {
+    return ones(head) + ' hundred and ' + convert(parseInt(tail.join()));
+  } else if (numString.length === 2) {
+    if (head === '1') {
+        return teens(tail[0]);
+    } else { 
+        return (tail === '0' ? '' : tens(head) + '-') + ones(tail[0]);
     }
-
+  } else {
+    return ones(head);
+  }
 };
 
 module.exports = convert;
--- js-numerals/src/convert.spec.js
+++ js-numerals/src/convert.spec.js
@@ -11,4 +11,28 @@ describe('Basic spec', () => {
     expect(convert(7)).to.equal('seven');
   });
 
+  it('convert function should work correctly #2', () => {
+    expect(convert(0)).to.equal('zero');
+  });
+
+  it('convert function should work correctly #3', () => {
+    expect(convert(42)).to.equal('forty-two');
+  });
+
+  it('convert function should work correctly #4', () => {
+    expect(convert(10)).to.equal('ten');
+  });
+
+  it('convert function should work correctly #5', () => {
+    expect(convert(11)).to.equal('eleven');
+  });
+
+  it('convert function should work correctly #6', () => {
+    expect(convert(19)).to.equal('nineteen');
+  });
+
+  it('convert function should work correctly #7', () => {
+    expect(convert(29)).to.equal('twenty-nine');
+  });
+
 });
