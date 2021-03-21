412f4689b77490c5d7778b46876d04d631024577
Zoltan Tamasi <mail@zoltantamasi.net>
2021-03-21 10:11:29 +0100

    Solved basic test case for 7.

--- js-numerals/src/convert.js
+++ js-numerals/src/convert.js
@@ -1,3 +1,30 @@
-const convert = (num) => num;
+
+const ones = (num) => (
+    num === '1' ? 'one' :
+    num === '2' ? 'two' :
+    num === '3' ? 'three' :
+    num === '4' ? 'four' :
+    num === '5' ? 'five' :
+    num === '6' ? 'six' :
+    num === '7' ? 'seven' :
+    num === '8' ? 'eight' :
+    num === '9' ? 'nine' : ''
+);
+
+const convert = (num) => {
+
+    const numString = num.toString().split('');
+    const [head, ...tail] = numString;
+
+    if (numString.length > 3) {
+    } else if (numString.length === 3) {
+        return ones(head) + ' hundred and ' + convert(tail);
+    } else if (numString.length === 2) {
+        return tens(head) + tail === '0' ? '' : '-' + ones(tail);
+    } else {
+        return ones(head);
+    }
+
+};
 
 module.exports = convert;
--- js-numerals/src/convert.spec.js
+++ js-numerals/src/convert.spec.js
@@ -6,4 +6,9 @@ describe('Basic spec', () => {
   it('convert function should exist', () => {
     expect(convert).not.to.be.undefined;
   });
+
+  it('convert function should work correctly #1', () => {
+    expect(convert(7)).to.equal('seven');
+  });
+
 });
