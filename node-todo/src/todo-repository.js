90de1d08ec05550ee985d17ffaee50cd9c28caea
Zoltan Tamasi <mail@zoltantamasi.net>
2021-03-25 11:52:08 +0100

    Can create and read todo entites.

--- node-todo/src/todo-repository.js
+++ node-todo/src/todo-repository.js
@@ -1,6 +1,19 @@
 
-const createTodo = () => {};
+const uuid = require('uuid');
+const todos = [];
+
+const createTodo = ({
+  text, priority, done
+}) => {
+  const newItem = { text, priority, done, id: uuid.v1() };
+  todos.push(newItem);
+  return newItem
+};
+
+const getTodos = () => {
+  return todos;
+};
 
 module.exports = {
-  createTodo
-}
+  createTodo, getTodos
+};
--- node-todo/src/todos.spec.js
+++ node-todo/src/todos.spec.js
@@ -1,9 +1,18 @@
 const { expect } = require('chai');
+const uuid = require('uuid');
 
 const todoRepository = require('./todo-repository');
 
 describe('Basic spec', () => {
   it('can create todo entity', () => {
     expect(todoRepository.createTodo).not.to.be.undefined;
+  });
+
+  it('can create and read back todo entity', () => {
+    const newItem = todoRepository.createTodo({ text: 'todoItem', priority: 3, done: false });
+    expect(newItem.text).to.equal('todoItem');
+    expect(newItem.priority).to.equal(3);
+    expect(newItem.done).to.equal(false);
+    expect(uuid.validate(newItem.id)).to.equal(true);
   });
 });
